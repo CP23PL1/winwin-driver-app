603669e3f6788586e7e1775804ec8b14828639e9
boy52hz <tharadon.saenmart@mail.kmutt.ac.th>
2023-12-11 13:56:07 +0700

    feat(auth): integrate auth0 user id with winwin api driver profile

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react'
 import { useFonts } from 'expo-font'
-import { Redirect, Slot, SplashScreen, Stack } from 'expo-router'
+import { Slot, SplashScreen } from 'expo-router'
 import { DesignSystem } from '../utils/design-system'
-import { Auth0Provider, useAuth0 } from 'react-native-auth0'
+import { Auth0Provider } from 'react-native-auth0'
 import { LoaderScreen } from 'react-native-ui-lib'
+
+import { QueryClientProvider } from '../providers/query-client'
 
 DesignSystem.setup()
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -32,11 +34,13 @@ export default function RootLayout() {
   }
 
   return (
-    <Auth0Provider
-      domain={process.env.EXPO_PUBLIC_AUTH0_DOMAIN!}
-      clientId={process.env.EXPO_PUBLIC_AUTH0_CLIENT_ID!}
-    >
-      <Slot />
-    </Auth0Provider>
+    <QueryClientProvider>
+      <Auth0Provider
+        domain={process.env.EXPO_PUBLIC_AUTH0_DOMAIN!}
+        clientId={process.env.EXPO_PUBLIC_AUTH0_CLIENT_ID!}
+      >
+        <Slot />
+      </Auth0Provider>
+    </QueryClientProvider>
   )
 }
