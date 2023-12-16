74c25953b39b9a22c7c60ddf6c80cf15fd1224c9
boy52hz <tharadon.saenmart@mail.kmutt.ac.th>
2023-12-16 18:26:10 +0700

    refactor: added constants folder and added zustand-x

--- stores/login-wizard.ts
+++ stores/login-wizard.ts
@@ -1,27 +1,7 @@
-import { create } from 'zustand'
+import { createStore } from 'zustand-x'
 
-type LoginWizardState = {
-  dialCode: string
-  phoneNumber: string | null
-}
+const loginWizardStore = createStore('loginWizard')({
+  phoneNumber: '',
+})
 
-type LoginWizardActions = {
-  getPhoneNumberWithGeo: () => string | null
-  setPhoneNumber: (phoneNumber: string) => void
-  reset: () => void
-}
-
-export type LoginWizardStore = LoginWizardState & LoginWizardActions
-
-export const useLoginWizardStore = create<LoginWizardStore>((set, get) => ({
-  dialCode: '+66', // Default to Thailand
-  phoneNumber: null,
-  getPhoneNumberWithGeo: () => {
-    const dialCode = get().dialCode
-    const phoneNumber = get()?.phoneNumber
-    if (!phoneNumber) return null
-    return `${dialCode}${phoneNumber}`
-  },
-  setPhoneNumber: (phoneNumber: string) => set({ phoneNumber }),
-  reset: () => set({ phoneNumber: null }),
-}))
+export default loginWizardStore
