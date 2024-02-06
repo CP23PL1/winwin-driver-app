b888b216c7a20b1ddedc0e228d9ecf8afae4d405
Siripong Phiwkhaw <72536609+siripongphiwkhaw@users.noreply.github.com>
2024-02-06 19:09:53 +0700

    fix: remove client sdk in useUser

--- hooks/useUser.ts
+++ hooks/useUser.ts
@@ -1,17 +1,17 @@
 import { useAuth0 } from 'react-native-auth0'
 import { useQueryClient } from 'react-query'
-import apis from '../apis'
+import {driversApi} from '../apis/drivers'
 import { useCallback, useEffect, useMemo, useState } from 'react'
-import { DriverDto } from '@cp23pl1/winwin-client-sdk'
+import { Driver } from '../apis/drivers/type'
 
 export function useUser() {
   const queryClient = useQueryClient()
   const { user, isLoading: isUserLoading } = useAuth0()
-  const [profile, setProfile] = useState<DriverDto | null>(null)
+  const [profile, setProfile] = useState<Driver | null>(null)
   const [isProfileLoading, setIsProfileLoading] = useState(false)
 
   const fetchDriverProfile = useCallback(async (userId: string) => {
-    const cachedDriverProfile = queryClient.getQueryData<DriverDto>(['driver', userId])
+    const cachedDriverProfile = queryClient.getQueryData<Driver>(['driver', userId])
     if (cachedDriverProfile) {
       console.log('get cached driver profile')
       setProfile(cachedDriverProfile)
@@ -22,7 +22,7 @@ export function useUser() {
 
     const driverProfile = await queryClient.fetchQuery({
       queryKey: ['driver', userId],
-      queryFn: () => apis.drivers.findOne(userId, 'uid').then((res) => res.data),
+      queryFn: () => driversApi.getDriverById(userId),
     })
     queryClient.setQueryData(['driver', userId], driverProfile)
     setProfile(driverProfile)
